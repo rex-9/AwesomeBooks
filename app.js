12f9e007fd83ad8ee5a645a592b8985a91054e27
Htet Naing <htetnaing0814@gmail.com>
2022-06-27 18:12:32 +0630

    basic styling

--- app.js
+++ app.js
@@ -64,24 +64,17 @@ submit.addEventListener('click', newBook, false);
 
 for (let i = 0; i < books.length; i += 1) {
   const book = document.createElement('li');
+  book.classList.add('book')
 
-  const titleElement = document.createElement('p');
-  titleElement.innerHTML = books[i].title;
-
-  const authorElement = document.createElement('p');
-  authorElement.innerHTML = books[i].author;
+  const labelElement = document.createElement('p');
+  labelElement.innerHTML = `"${books[i].title}" by ${books[i].author}`;
 
   const remove = document.createElement('button');
   remove.innerHTML = 'Remove';
   remove.addEventListener('click', removeBook, false);
   remove.index = i;
 
-  const divider = document.createElement('div');
-  divider.classList.add('divider');
-
-  book.appendChild(titleElement);
-  book.appendChild(authorElement);
+  book.appendChild(labelElement);
   book.appendChild(remove);
-  book.appendChild(divider);
   list.appendChild(book);
 }
