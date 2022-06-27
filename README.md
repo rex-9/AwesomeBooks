ffe6bb240ef04f4c651374e9f62a2cc939d392ac
Htet Naing <htetnaing0814@gmail.com>
2022-06-27 14:18:39 +0630

    collection of books and adding items

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# awesome-books
+# Project Name
+Awesome books
+
+## Built With
+- HTML
+- CSS
+- JS
+## Live Demo
+
+[Awesome books](https://htetnaing0814.github.io/awesome-books/)
+
+## Getting Started
+
+To get a local copy up and running:
+
+1. Clone this repository or download the zip folder:
+
+**``git clone https://github.com/HtetNaing0814/awesome-books.git``**
+
+2. Navigate to the location of the folder in your machine:
+
+**``you@your-Pc-name:~$ cd <folder>``**
+
+To get a local copy up and running follow these simple example steps.
+
+### Deployment
+
+Here is the online version of [Awesome Books](https://htetnaing0814.github.io/awesome-books/)
+
+## Author
+👤 - Github: [@HtetNaing0814](https://github.com/HtetNaing0814)
+
+## 🤝 Contributing
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check [issue page](https://github.com/HtetNaing0814/awesome-books/issues).
+
+## Show your support
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+[Microverse](https://bit.ly/MicroverseTN)
+
+## 📝 License
+This project is MIT licensed.
---
+++ app.js
@@ -0,0 +1,67 @@
+let bookObj = {
+    title: '',
+    author: ''
+};
+
+var list = document.getElementById("list");
+var title = document.getElementById("title");
+var author = document.getElementById("author");
+var submit = document.getElementById("btn");
+
+function addItem(e) {
+    const books = JSON.parse(localStorage.getItem('books'));
+    bookObj = {
+        title: title.value,
+        author: author.value
+    }
+    books.push(bookObj);
+    localStorage.setItem('books', JSON.stringify(books));
+    console.log(books);
+    window.setTimeout(function () {
+        window.location.reload();
+    }, 500);
+}
+
+function removeItem(e) {
+    var elElement = e.currentTarget;
+    var book = elElement.parentElement;
+    while (book.firstChild) {
+        book.removeChild(book.firstChild);
+    }
+    const index = books.indexOf(i);
+    console.log(e);
+    books.splice(index, 1); // 2nd parameter means remove one item only
+    localStorage.setItem('books', JSON.stringify(books));
+    console.log(books);
+}
+
+submit.addEventListener("click", addItem, false);
+
+if (!localStorage.getItem('books')) {
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
+const books = JSON.parse(localStorage.getItem('books'));
+for (let i = 0; i < books.length; i++) {
+    var book = document.createElement("li");
+
+    var titleElement = document.createElement("p");
+    titleElement.innerHTML = books[i].title;
+
+    var authorElement = document.createElement("p");
+    authorElement.innerHTML = books[i].author;
+
+    var remove = document.createElement("button");
+    remove.innerHTML = "Remove";
+    remove.addEventListener("click", removeItem.bind(i), false);
+
+    var divider = document.createElement("div");
+    divider.classList.add('divider');
+
+    book.appendChild(titleElement);
+    book.appendChild(authorElement);
+    book.appendChild(remove);
+    book.appendChild(divider);
+    list.appendChild(book);
+}
+console.log(books);
